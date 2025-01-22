2c4cbd20508b3d1c6902ded432844715e26e6912
Moony <moony25@proton.me>
2025-01-22 02:20:13 +0200

    A lot of stuff

--- include/strlib.h
+++ include/strlib.h
@@ -3,21 +3,20 @@
 
 #include <stddef.h>
 
-size_t strlen(const char *str); //COMPLETED
-char* strcpy(char *dest, const char *src); //COMPLETED
-char* strncpy(char *dest, const char *src, size_t n); //COMPLETED
-int bstr(const char *str1, const char *str2); //COMPLETED
-int strcmp(const char *str1, const char *str2); //COMPLETED
-int strncmp(const char *str1, const char *str2, size_t n); //COMPLETED
-/*int astrcmp(const char *str1, const char *str2); SCRAPPED
-int nastrcmp(const char *str1, const char *str2);  SCRAPPED */
-char* strcat(char *dest, const char *src); // COMPLETED
-char* strncat(char *dest, const char *src, size_t n); // COMPLETED
-char* strdup(const char *src); // COMPLETED
-char* strchr(const char *str, int c); // COMPLETED
-char* strnchr(const char *str, int c); // COMPLETED
-char* strrchr(const char *str, int c);
-char* strstr(const char *haystack, const char *needle);
+size_t strlen(const char* str);
+char* strcpy(char* dest, char* src);
+char* strncpy(char* dest, char* src, const size_t n);
+int cmpstr(char* str1, char* str2);
+int strdiff(const char* str1, const char* str2);
+int strdiffn(const char* str1, const char* str2, const size_t n);
+char* strcat(const char* dest, const char* src);
+char* strncat(const char* dest, const char* src, const size_t n);
+char* strdup(const char* src);
+char* strchr(char* str, const int c);
+char* strnchr(char* str, int int c, const size_t n);
+char* strrchr(const char* str, const int c);
+int strstr(const char* haystack, const char* needle);
+int bstr(const char* str1, const char* str2); // not completed here and below
 
 void* malloc(size_t size);
 void free(void *ptr);
@@ -25,8 +24,8 @@ void* realloc(void *ptr, size_t new_size);
 void* calloc(size_t num, size_t size);
 void exit(int status);
 
-int atoi(const char *str);
-char* itoa(int num, char *str, int base);
+int atoi(const char* str);
+char* itoa(int num, char* str, int base);
 
 int abs(int x);
 void qsort(void *base, size_t num_elements, size_t element_size, int (*cmp)(const void *, const void *));
--- src/strlib.c
+++ src/strlib.c
@@ -1,6 +1,6 @@
 /*
  * DISCLAIMER:
- * OSD is not responsible for any damages caused by buffer overflow or improper usage.
+ * OSD is not responsible for any damages caused by memory leaks, buffer overflows, etc.
  * The user is responsible for ensuring that input strings are properly null-terminated.
  * 
  * SOURCE FILE FOR strlib (strlib.c)
@@ -11,41 +11,39 @@
  * This code is free software; you can redistribute it and/or modify it!
  */
 
-// TODO: nstrcat
-
 #include "strlib.h"
 
-// Get the length of a string
-size_t strlen(const char *str) {
-    size_t length = 0;
-    while (str[length] != '\0')
-      length++;
-    return length;
-} //VALID
+// Return the length of [str].
+size_t strlen(const char* str) {
+  size_t length = 0;
+  while (str[length] != '\0')
+    length++;
+  return length;
+}
 
-// Copy string to a destination
-char* strcpy(char *dest, const char *src) {
+// Copy [src] to [dest] and return [dest] as the end of the string.
+char* strcpy(char* dest, char* src) {
   while(*src != '\0') {
     *dest = *src;
     dest++, src++;
   }
   *dest = '\0'
   return dest;
-} //VALID
+}
 
-// Copy string characters to a destination N times
-char* strncpy(char *dest, const char *src, size_t n) {
-  size_t numTimes = 0;
-  while(*src != '\0' && n > numTimes) {
+// Copy at most [n] characters from [src] to [dest].
+char* strncpy(char* dest, char* src, const size_t n) {
+  size_t count = 0;
+  while(*src != '\0' && count < n) {
     *dest = *src;
-    dest++, src++, numTimes++;
+    dest++, src++, count++;
   }
   *dest = '\0'
   return dest;
-} //VALID
+}
 
-// Return a integer based on the size biased towards str1
-int bstr(const char *str1, const char *str2) {
+// Return 0, 1 or -1 if [str1]'s length is =, > or < relative to [str2]'s, respectfully.
+int cmpstr(char* str1, char* str2) {
   while (*str1 != '\0' && *str2 != '\0')
     str1++, str2++;
   if (*str1 == *str2)  // equal
@@ -53,76 +51,92 @@ int bstr(const char *str1, const char *str2) {
   if (*str1 == '\0')  // str1 < str2
     return -1;
   return 1;  // str1 > str2
-} //VALID
+}
 
-// Get the difference in length between the strings.
-size_t strcmp(const char *str1, const char *str2) {
+// Return the absolute difference in length between the strings.
+size_t strdiff(const char* str1, const char* str2) {
   size_t str1Len = strlen(str1);
   size_t str2Len = strlen(str2);
   if (str1Len > str2Len)
     return str1Len - str2Len;
   return str2Len - str1Len;
-} //VALID
-
-// Get the difference in length between the strings N times, return -1 for error
-size_t strncmp(const char *str1, const char *str2, size_t n) {
-  size_t str1Len = strlen(str1);
-  size_t str2Len = strlen(str2);
-  if ((str1Len - str2Len - n) >= 0) return str1Len - str2len - n;
-  if ((str2Len - str1Len - n) >= 0) return str2Len - str1Len - n;
-  return -1;
-} //VALID
-
-// add string to string
-char* strcat(const char *src, char *dest) {
-  char *pDestEnd = dest + strlen(dest);
-  size_t i = 0;
-  for (; src[i] != '\0'; i++)
-    pDestEnd[i] = src[i];
-  pDestEnd[i] = '\0';
-  return dest;
-} //VALID
-
-// add string to string N times
-char* strncat(const char *src, char *dest, size_t n) {
-  char *pDestEnd = dest + strlen(dest);
-  size_t i = 0;
-  for (; src[i] != '\0' && n > i; i++)
-    pDestEnd[i] = src[i];
-  pDestEnd[i] = '\0';
-  return dest;
-} //VALID
-
-//Allocate a duplicated string
-char* strdup(const char *src) {
-  size_t len = strlen(*src) + 1;
-  char *dup = malloc(len);
-  strcpy(dup, src);
-  return dup;
-} //VALID
-
-//Return a pointer to the first occurence of c in str
-char* strchr(const char *str, int c) {
-  while (*str != '\0' && *str != c)
-    str++;
-  return str++;
-} //VALID
-
-//Return a pointer to the first occurence of c in str N times
-char* strnchr(const char *str, int c, size_t n) {
-  size_t numTimes = 0;
-  while (*str != '\0' && *str != c && n > numTimes)
-    str++, numTimes++;
-  return str++;
 }
 
-/*char* strstr(const char *haystack, const char *needle) {
-  //search string at base, search needle size inbetween the base
-  const size_t needleSize = strlen(needle) - 1;
-  const pOccurence = 0;
-  while (*haystack != '\0') {
-    haystack
-  }
-}/*
+// Return the absolute difference in length between the strings, offset by [n]; SiZE_MAX when [n] > strdiff(str1, str2).
+size_t strdiffn(const char* str1, const char* str2, const size_t n) {
+  size_t diff = strdiff(str1, str2);
+  if (diff >= n)
+    return diff - n;
+  return SIZE_MAX;
+}
 
+// Append [src] at the end of [dest] and return [dest].
+char* strcat(const char* src, const char* dest) {
+  strcpy(dest + strlen(dest), src);
+  return dest;
+}
+
+// Append [src] at the end of [dest], [n] times and return new string.
+char* strncat(const char* src, const char* dest, const size_t n) {
+  size_t destLen = strlen(dest);
+  size_t srcLen = strlen(src);
+  char* pNew = malloc(destLen + srcLen * n);
+
+  strcpy(pNew, dest);
+  for (size_t i = 0; i < n; i++)
+    strcpy(pNew + destLen + srcLen * i, scr);
+
+  return pNew;
+}
+
+// Allocate a duplicate of [src] and return it. 
+char* strdup(const char* src) {
+  char* dup = malloc(strlen(src));
+  strcpy(dup, src);
+  return dup;
+}
+
+// Return a pointer to the first occurence of [c] in [str], end of the string if not found.
+char* strchr(char* str, const int c) {
+  while (*str != '\0' && *str != c)
+    str++;
+  return str;
+}
+
+// Return a pointer to the [n]th occurance of [c] in [str], end of the string if not found.
+char* strnchr(char* str, const int c, const size_t n) {
+  size_t count = 0;
+  while (*str != '\0' && count != n) {
+    str++;
+    if (*str == c)
+      count++;
+  }
+  
+  return str;
+}
+
+// Return a pointer to the last occurance of [c] in [str], end of the string if not found.
+char* strrstr(char* str, const int c) {
+  char* lastFound = NULL;
+
+  while (*str != '\0') {
+    str++;
+    if (*str == c)
+      lastFound = str;
+  }
+
+  return (lastFound == NULL) ? str : lastFound;
+}
+
+// Returns a pointer of the first occurance of [needle] in [haystack], otherwise -1.
+int strstr(const char* haystack, const char* needle) {
+  size_t i = 0;
+  for (size_t i = 0; haystack[i] != '\0'; i++) {
+    for (size_t j = 0; haystack[i + j] == needle[j]; j++)
+      if (needle[j + 1] == '\0')  // the whole of [needle] matched
+        return i + j + 1;
+  }
+
+  return -1;
+}
 
